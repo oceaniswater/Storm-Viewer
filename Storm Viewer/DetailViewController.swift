2d4cc5dd379e6d1bea51082523673f86e0808c9d
markgolubev <markflyingbear@gmail.com>
2022-12-08 18:26:10 +0000

    Learned about table views, image views, app bundles, FileManager, typecasting, view controllers, storyboards, outlets, Auto Layout, UI Image

---
+++ Storm Viewer/DetailViewController.swift
@@ -0,0 +1,51 @@
+//
+//  DetailViewController.swift
+//  Storm Viewer
+//
+//  Created by Марк Голубев on 08.12.2022.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+    
+    var selectedImage: String?
+    var countOfImages = 0
+    var positionOfImages = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "\(positionOfImages) of \(countOfImages)"
+        
+        navigationItem.largeTitleDisplayMode = .never
+        // switch off large title mode for this view
+        
+
+        if let imageToLoad = selectedImage {
+            imageView.image = UIImage(named: imageToLoad)
+        }
+        // work with image
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
+    }
+    // hide bar
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Storm Viewer/ViewController.swift
+++ Storm Viewer/ViewController.swift
@@ -7,13 +7,58 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    var pictures = [String]()
+    // array of pictures
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        title = "Storm Viewer"
+        // title for View
+        navigationController?.navigationBar.prefersLargeTitles = true
+        // make large title for all of views in navigation stack
+        
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        // work with file manager
+        
+        for item in items {
+            if item.hasPrefix("nssl") {
+                // this is a picture load!
+                pictures.append(item)
+            }
+        }
+        pictures.sort()
     }
-
-
+    
+    // MARK: - Count of rows in table
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    // MARK: - Create and setup reusable cell
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
+    }
+    
+    // MARK: - Transfer data to other VC after selecting row
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
+            // it is typecasting
+            vc.selectedImage = pictures[indexPath.row]
+            vc.countOfImages = pictures.count
+            vc.positionOfImages = indexPath.row + 1
+//            vc.positionOfImages = pictures[indexPath.row]
+            navigationController?.pushViewController(vc, animated: true)
+            
+        }
+    }
 }
 
