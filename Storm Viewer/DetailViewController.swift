f35c4cda37b2fd601ac4ab032b88c378f985e593
markgolubev <markflyingbear@gmail.com>
2022-12-12 14:49:27 +0000

    added UIBarButtonItem and UIActivityViewController for sharing and saving image

--- Storm Viewer/DetailViewController.swift
+++ Storm Viewer/DetailViewController.swift
@@ -22,6 +22,9 @@ class DetailViewController: UIViewController {
         navigationItem.largeTitleDisplayMode = .never
         // switch off large title mode for this view
         
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
+        // add
+        
 
         if let imageToLoad = selectedImage {
             imageView.image = UIImage(named: imageToLoad)
@@ -36,6 +39,16 @@ class DetailViewController: UIViewController {
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         navigationController?.hidesBarsOnTap = false
+    }
+    
+    @objc func shareTapped() {
+        guard let image  = imageView.image?.jpegData(compressionQuality: 0.8) else {
+            print("No image found")
+            return
+        }
+        let vc = UIActivityViewController(activityItems: [image], applicationActivities: [])
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(vc, animated: true)
     }
     // hide bar
     /*
